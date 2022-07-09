717935a8b761f5a2f44950e836428ca331d08503
jmeno1011 <jwhltn8282@gmail.com>
2022-07-09 22:32:51 +0900

    4-1

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 import { ApolloServer, gql } from "apollo-server";
 
-const server = new ApolloServer({
+// SDL : Schema Definition Language
+// 유저에게 뭔가 보내주려면 타입을 설정해줘야한다.
+const typeDefs = gql`
+  type Query {
+    text: String
+  }
+`;
 
-})
+const server = new ApolloServer({ typeDefs });
 
-server.listen().then(({url}))=>{
-    console.log(`Running on ${url}`)
-}
+server.listen().then(({ url }) => {
+  console.log(`Running on ${url}`);
+});
