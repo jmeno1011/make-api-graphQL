1c85a93741b5ca8e14137ef5241518c0299a6edf
jmeno1011 <jwhltn8282@gmail.com>
2022-07-10 11:47:12 +0900

    4-3

--- server.js
+++ server.js
@@ -3,8 +3,21 @@ import { ApolloServer, gql } from "apollo-server";
 // SDL : Schema Definition Language
 // 유저에게 뭔가 보내주려면 타입을 설정해줘야한다.
 const typeDefs = gql`
-  type Query {
+  type User {
+    id: ID
+    username: String
+  }
+  type Tweet {
+    id: ID
     text: String
+    author: User
+  }
+  type Query {
+    allTweets: [Tweet]
+    tweet(id: ID): Tweet
+  }
+  type Mutation {
+    postTweet(text: String, userId: ID): Tweet
   }
 `;
 
