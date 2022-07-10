8a151b86494ec18bc7c0fa754c42e24fbf65b2a3
jmeno1011 <jwhltn8282@gmail.com>
2022-07-10 15:25:38 +0900

    4-5

--- server.js
+++ server.js
@@ -4,20 +4,23 @@ import { ApolloServer, gql } from "apollo-server";
 // 유저에게 뭔가 보내주려면 타입을 설정해줘야한다.
 const typeDefs = gql`
   type User {
-    id: ID
-    username: String
+    id: ID!
+    username: String!
+    fisrtName: String!
+    lastName: String!
   }
   type Tweet {
-    id: ID
-    text: String
-    author: User
+    id: ID!
+    text: String!
+    author: User!
   }
   type Query {
-    allTweets: [Tweet]
-    tweet(id: ID): Tweet
+    allTweets: [Tweet!]!
+    tweet(id: ID!): Tweet
   }
   type Mutation {
-    postTweet(text: String, userId: ID): Tweet
+    postTweet(text: String!, userId: ID!): Tweet!
+    deleteTweet(id: ID!): Boolean!
   }
 `;
 
