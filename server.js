81d5ea9b3d5b3b3958422ca8b0cafc9c0617d4f5
jmeno1011 <jwhltn8282@gmail.com>
2022-07-10 15:46:53 +0900

    4-6

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
 import { ApolloServer, gql } from "apollo-server";
 
+const tweets = [
+  {
+    id: "1",
+    text: "first one",
+  },
+  {
+    id: "2",
+    text: "second one",
+  },
+  {
+    id: "3",
+    text: "third one",
+  },
+];
+
 // SDL : Schema Definition Language
 // 유저에게 뭔가 보내주려면 타입을 설정해줘야한다.
 const typeDefs = gql`
@@ -24,7 +39,18 @@ const typeDefs = gql`
   }
 `;
 
-const server = new ApolloServer({ typeDefs });
+const resolvers = {
+  Query: {
+    allTweets() {
+      return tweets;
+    },
+    tweet(root, { id }) {
+      return tweets.find((tweet) => tweet.id === id);
+    },
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen().then(({ url }) => {
   console.log(`Running on ${url}`);
